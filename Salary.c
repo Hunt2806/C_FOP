ad90180e1e353b587c55e8c979e79f9d8a8efd01
Aarush Verma <leoaarush@gmail.com>
2026-02-12 08:07:53 +0530

    Add Salary calculation program in Salary.c

---
+++ Salary.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    float basic, hra, ta, gross, tax, net;
+
+    printf("Enter Basic Pay: ");
+    scanf("%f", &basic);
+
+    hra = 0.10 * basic;
+    ta  = 0.05 * basic;
+
+    gross = basic + hra + ta;
+
+    tax = 0.02 * gross;
+
+    net = gross - tax;
+
+    printf("\nBasic Pay = %.2f", basic);
+    printf("\nHRA (10%%) = %.2f", hra);
+    printf("\nTA (5%%) = %.2f", ta);
+    printf("\nGross Salary = %.2f", gross);
+    printf("\nProfessional Tax (2%%) = %.2f", tax);
+    printf("\nNet Salary = %.2f", net);
+
+    return 0;
+}
