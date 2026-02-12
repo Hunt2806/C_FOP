e613beaacf805d6d360cad9ae270ebe677770af3
Aarush Verma <leoaarush@gmail.com>
2026-02-12 08:10:18 +0530

    Fix conditional structure in Grades.c

---
+++ Grades.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    float m1, m2, m3, m4, m5, total, percentage;
+
+    printf("Enter marks of 5 courses: ");
+    scanf("%f %f %f %f %f", &m1, &m2, &m3, &m4, &m5);
+
+    if (m1 < 40 || m2 < 40 || m3 < 40 || m4 < 40 || m5 < 40) {
+        printf("FAIL");
+    } 
+    
+    else {
+        total = m1 + m2 + m3 + m4 + m5;
+        percentage = (total / 500) * 100;
+
+        printf("PASS\n");
+        printf("Aggregate Percentage = %.2f\n", percentage);
+
+        if (percentage >= 75)
+            printf("Grade: Distinction");
+        else if (percentage >= 60)
+            printf("Grade: First Division");
+        else if (percentage >= 50)
+            printf("Grade: Second Division");
+        else
+            printf("Grade: Third Division");
+    }
+
+    return 0;
+}
